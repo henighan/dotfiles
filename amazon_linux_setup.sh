3d785ddbff2c7a4cd539a6c0d4401b94adaf33ac
Tom Henighan <henighan.2@gmail.com>
2017-07-29 10:44:22 -0400

    added amazon linux setup script

--- amazon_linux_setup.sh
+++ amazon_linux_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo yum install tmux
 git clone https://github.com/henighan/dotfiles.git ~/
 cp .vimrc ~/
 cp -R .vim/ ~/.vim
@@ -7,4 +8,3 @@ cp .tmux.conf ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt-get install xclip
