4f852bf83e9855f0747c5aa5a811ce8406bb45e2
Tom Henighan <henighan.2@gmail.com>
2017-07-29 10:42:17 -0400

    removed xclip installation

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,4 +7,4 @@ cp .tmux.conf ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt-get install xclip
+# sudo apt-get install xclip
