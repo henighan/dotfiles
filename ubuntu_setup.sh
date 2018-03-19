3624dbdf807a611571b6ef3989904c61428f1333
Tom Henighan <henighan.2@gmail.com>
2018-03-19 13:47:42 -0700

    added zshell and oh my zshell to ubuntu setup script

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8,3 +8,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt-get install xclip
+sudo apt-get install zsh
+sudo chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
