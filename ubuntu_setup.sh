977d0b35bfd81a932dc2f51f33132b1ec98146e9
Tom Henighan <henighan.2@gmail.com>
2018-03-19 14:12:08 -0700

    removed changing of default shell

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,5 +7,4 @@ vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt-get install xclip
 sudo apt-get install zsh
-sudo chsh --s $(which zsh)
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
