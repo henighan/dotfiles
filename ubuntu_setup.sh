f93e6a0aa6268df5cdb0ed12e117f6ac3f25d644
Tom Henighan <henighan.2@gmail.com>
2018-03-19 14:09:17 -0700

    added sudo for installing oh my zshell

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/henighan/dotfiles.git ~/
-cp .vimrc ~/
-cp -R .vim/ ~/.vim
-cp .tmux.conf ~/
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt-get install xclip
 sudo apt-get install zsh
-sudo chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo chsh --s $(which zsh)
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
