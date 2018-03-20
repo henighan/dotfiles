a764df0b4501701600503398ab10c9f7a45b6fd4
Tom Henighan <henighan.2@gmail.com>
2018-03-19 18:05:38 -0700

    added installation of git flow

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8,3 +8,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt-get install xclip
 sudo apt-get install zsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install git-flow
