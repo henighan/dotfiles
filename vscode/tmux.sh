8f0dbb85451fbc44da416159e0985db0b0ace9f3
Tom Henighan <henighan@anthropic.com>
2021-09-02 17:05:28 -0700

    wip

--- vscode/tmux.sh
+++ vscode/tmux.sh
@@ -4,8 +4,12 @@ SESSION="vscode`pwd`"
 if ! tmux attach-session -d -t $SESSION; then
     tmux new-session -d -s $SESSION
     tmux split-window -h -t $SESSION
+    tmux new-window -t $SESSION
+    tmux split-window -v -t $SESSION
+    tmux split-window -h -t $SESSION
+    tmux select-pane -U
+    tmux split-window -h -t $SESSION
+    tmux select-window -t 0
+    tmux select-pane -t 0
     tmux attach-session -d -t $SESSION
-    # echo "Hello"
-    # tmux send-keys -t $SESSION -X cursor-left
-    # tmux select-pane -t 1
 fi
