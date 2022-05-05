3047a373470c00ec29883328192bf830b5f78090
Tom Henighan <henighan@anthropic.com>
2022-05-05 10:02:45 -0700

    now need to specify full tmux path for some reason, perhaps we're not sourcing .zshrc or something

--- vscode/tmux.sh
+++ vscode/tmux.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 SESSION="vscode`pwd`"
+echo $SESSION
+echo $SHELL
 
-if ! tmux attach-session -d -t $SESSION; then
-    tmux new-session -d -s $SESSION
-    tmux split-window -h -t $SESSION
-    tmux new-window -t $SESSION
-    tmux split-window -v -t $SESSION
-    tmux split-window -h -t $SESSION
-    tmux select-pane -U -t $SESSION
-    tmux split-window -h -t $SESSION
-    tmux -t $SESSION select-window -t 0 
-    tmux -t $SESSION select-pane -t 0
-    tmux attach-session -d -t $SESSION
+
+if ! /opt/homebrew/bin/tmux attach-session -d -t $SESSION; then
+    /opt/homebrew/bin/tmux new-session -d -s $SESSION
+    /opt/homebrew/bin/tmux split-window -h -t $SESSION
+    /opt/homebrew/bin/tmux new-window -t $SESSION
+    /opt/homebrew/bin/tmux split-window -v -t $SESSION
+    /opt/homebrew/bin/tmux split-window -h -t $SESSION
+    /opt/homebrew/bin/tmux select-pane -U -t $SESSION
+    /opt/homebrew/bin/tmux split-window -h -t $SESSION
+    /opt/homebrew/bin/tmux -t $SESSION select-window -t 0 
+    /opt/homebrew/bin/tmux -t $SESSION select-pane -t 0
+    /opt/homebrew/bin/tmux attach-session -d -t $SESSION
 fi
